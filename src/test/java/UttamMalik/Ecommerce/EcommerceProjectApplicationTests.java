cf0307db5cad1919e4f47322ade7d2217c960326
Uttam Malik <65278295+UttamMalik@users.noreply.github.com>
2023-03-26 15:02:31 +0530

    Initially structure with views

---
+++ src/test/java/UttamMalik/Ecommerce/EcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package UttamMalik.Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
